229aa641b057ea284b7ebfe550d177e208ef2c05
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-03-27 10:46:02 +0100

    Lagt til UnitTest i referanser

---
+++ Digipost.Api.Client.Tests/XmlGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Digipost.Api.Client.Tests
+{
+    
+    [TestClass]
+    public class XmlGeneratorTests
+    {
+
+        [ClassInitialize]
+        public static void ClassInit()
+        {
+            
+        }
+
+        
+        [TestMethod]
+        public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+        {
+         
+        }
+
+
+
+    }
+}

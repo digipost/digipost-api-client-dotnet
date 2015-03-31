41142df8631aa398d5f13fdcd9f8af8530a4b6f5
Kristian Enge <kristian.ienge@gmail.com>
2015-03-31 11:53:40 +0200

    serialize class to xml

--- Digipost.Api.Client.Testklient/Program.cs
+++ Digipost.Api.Client.Testklient/Program.cs
@@ -4,19 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Digipost.Api.Client.Testklient
 {
     class Program
     {
         private static string DocumentGuid = Guid.NewGuid().ToString();//'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
-        private static string xmlMessage = GetXmlMessage(DocumentGuid);
+        private static string xmlMessage;
         private static byte[] attachment = GetAttachment();
         private static string technicalSenderId = "779052"; //106768801
 
         static void Main(string[] args)
         {
- 
+            xmlMessage = getXmlFromClass(DocumentGuid);
             var t = DigipostApi.Send(technicalSenderId, "forsendelseId", "Digipostadresse", "Emne", xmlMessage, attachment, DocumentGuid);
             var r = t.Result;
         }
@@ -27,14 +29,32 @@ namespace Digipost.Api.Client.Testklient
             return File.ReadAllBytes(path);
         }
 
-        private static string GetXmlMessage(String docID)
+        private static string getXmlFromClass(String docId)
         {
+            //primary document
+            document doc = new document();
+            doc.authenticationlevel =authenticationlevel.PASSWORD;
+            doc.authenticationlevelSpecified = doc.authenticationlevel != null;
+            doc.sensitivitylevel = sensitivitylevel.NORMAL;
+            doc.sensitivitylevelSpecified  = doc.sensitivitylevel != null;
+            
+            doc.filetype = "txt";
+            doc.subject = "test subject";
+            doc.uuid = docId;
+            
+            //recipient
+            messagerecipient mr = new messagerecipient();
+            mr.ItemElementName = ItemChoiceType1.personalidentificationnumber;
+            mr.Item = "31108446911";
+            
+            //message
+            message m = new message();
+            m.primarydocument = doc;
+            m.recipient = mr;
 
-            return
-                string.Format("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><message xmlns=\"http://api.digipost.no/schema/v6\"><recipient>" +
-                              "<personal-identification-number>31108446911</personal-identification-number></recipient>" +
-                              "<primary-document><uuid>{0}</uuid><subject>Dokumentets emne</subject><file-type>txt</file-type><sms-notification/><authentication-level>PASSWORD</authentication-level><sensitivity-level>NORMAL</sensitivity-level>" +
-                              "</primary-document></message>", docID);
+            return SerializeUtil.Serialize(m);
+
         }
+
     }
 }
---
+++ Digipost.Api.Client/SerializeUtil.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Digipost.Api.Client
+{
+    class SerializeUtil
+    {
+        private sealed class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding { get { return Encoding.UTF8; } }
+        }
+        public static string Serialize<T>(T value)
+        {
+
+            if (value == null)
+            {
+                return null;
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = new UTF8Encoding(false);
+            settings.ConformanceLevel = ConformanceLevel.Document;
+            settings.Indent = false;
+            settings.OmitXmlDeclaration = false;
+
+            using (Utf8StringWriter textWriter = new Utf8StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    serializer.Serialize(xmlWriter, value);
+                }
+                return textWriter.ToString();
+            }
+        }
+
+        public static T Deserialize<T>(string xml)
+        {
+
+            if (string.IsNullOrEmpty(xml))
+            {
+                return default(T);
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            // No settings need modifying here
+
+            using (StringReader textReader = new StringReader(xml))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(textReader, settings))
+                {
+                    return (T)serializer.Deserialize(xmlReader);
+                }
+            }
+        }
+    }
+}

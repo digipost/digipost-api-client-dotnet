64839ca4d322e3304433c58cb27b893715850963
Kristian Enge <kristian.ienge@gmail.com>
2015-04-08 17:02:39 +0200

    Scaffolded the authFilter

---
+++ Digipost.Api.Client/AuthFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digipost.Api.Client
+{
+    public class AuthFilter : IHttpFilter
+    {
+        private IHttpFilter innerFilter;
+
+        public AuthFilter(IHttpFilter innerFilter)
+        {
+            if (innerFilter == null)
+            {
+                throw new ArgumentException("innerFilter cannot be null.");
+            }
+            this.innerFilter = innerFilter;
+        }
+
+        public IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> SendRequestAsync(HttpRequestMessage request)
+        {
+            return AsyncInfo.Run<HttpResponseMessage, HttpProgress>(async (cancellationToken, progress) =>
+            {
+                request.Headers.Add("Custom-Header", "CustomRequestValue");
+                HttpResponseMessage response = await innerFilter.SendRequestAsync(request).AsTask(cancellationToken, progress);
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                response.Headers.Add("Custom-Header", "CustomResponseValue");
+                return response;
+            });
+        }
+
+        public void Dispose()
+        {
+            innerFilter.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}

95f2d57625735a4caeaee70416619ce70e2289ec
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-15 13:11:23 +0200

    Changed to using prod server

--- Digipost.Api.Client.Testklient/Program.cs
+++ Digipost.Api.Client.Testklient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Security.Cryptography.X509Certificates;
 using ApiClientShared;
 using ApiClientShared.Enums;
@@ -11,7 +12,7 @@ namespace Digipost.Api.Client.Testklient
 {
     internal class Program
     {
-        private const string TechnicalSenderId = "106768801";
+        private const string TechnicalSenderId = "779052";  //"106768801";
         private static ResourceUtility _resourceUtility = new ResourceUtility("Digipost.Api.Client.Testklient.Resources");
 
         private static void Main(string[] args)
@@ -20,7 +21,7 @@ namespace Digipost.Api.Client.Testklient
             var config = new ClientConfig(TechnicalSenderId);
             Logging.Initialize(config);
 
-            var api = new DigipostApi(config,GetCert());
+            var api = new DigipostApi(config, GetCert());
             var t = api.Send(message);
 
             var r = t.Result;
@@ -33,50 +34,49 @@ namespace Digipost.Api.Client.Testklient
             var doc = new Document();
             doc.Authenticationlevel = AuthenticationLevel.Password;
             doc.Sensitivitylevel = SensitivityLevel.Normal;
-            doc.FileType = "pdf";
+            doc.FileType = "txt";
             doc.Subject = "test";
             doc.Uuid = Guid.NewGuid().ToString();
             doc.Content = GetPrimaryDocument();
 
 
-            //attachment1
-            var attachment1 = new Document();
-            attachment1.Authenticationlevel = AuthenticationLevel.Password;
-            attachment1.Sensitivitylevel = SensitivityLevel.Normal;
-            attachment1.FileType = "pdf";
-            attachment1.Subject = "attachment";
-            attachment1.Uuid = Guid.NewGuid().ToString();
-            attachment1.Content = GetAttachment();
+            ////attachment1
+            //var attachment1 = new Document();
+            //attachment1.Authenticationlevel = AuthenticationLevel.Password;
+            //attachment1.Sensitivitylevel = SensitivityLevel.Normal;
+            //attachment1.FileType = "pdf";
+            //attachment1.Subject = "attachment";
+            //attachment1.Uuid = Guid.NewGuid().ToString();
+            //attachment1.Content = GetAttachment();
 
-            //attachment2
-            var attachment2 = new Document();
-            attachment2.Authenticationlevel = AuthenticationLevel.Password;
-            attachment2.Sensitivitylevel = SensitivityLevel.Normal;
-            attachment2.FileType = "pdf";
-            attachment2.Subject = "attachment";
-            attachment2.Uuid = Guid.NewGuid().ToString();
-            attachment2.Content = GetAttachment();
+            ////attachment2
+            //var attachment2 = new Document();
+            //attachment2.Authenticationlevel = AuthenticationLevel.Password;
+            //attachment2.Sensitivitylevel = SensitivityLevel.Normal;
+            //attachment2.FileType = "pdf";
+            //attachment2.Subject = "attachment";
+            //attachment2.Uuid = Guid.NewGuid().ToString();
+            //attachment2.Content = GetAttachment();
 
             //recipient
             var mr = new MessageRecipient();
             mr.IdentificationType = IdentificationChoice.PersonalidentificationNumber;
-            mr.IdentificationValue = "01013300001";
+            mr.IdentificationValue = "31108446911";
 
             //message
             var m = new Message();
             m.PrimaryDocument = doc;
-            m.Attachment = new Document[2];
-            m.Attachment[0] = attachment1;
-            m.Attachment[1] = attachment2;
+            //m.Attachment = new Document[2];
+            //m.Attachment[0] = attachment1;
+            //m.Attachment[1] = attachment2;
             m.Recipient = mr;
-
 
             return m;
         }
 
         private static byte[] GetPrimaryDocument()
         {
-            return _resourceUtility.ReadAllBytes(true ,"Hoveddokument.txt");
+            return _resourceUtility.ReadAllBytes(true, "Hoveddokument.txt");
 
         }
 
@@ -87,9 +87,9 @@ namespace Digipost.Api.Client.Testklient
 
         private static X509Certificate2 GetCert()
         {
-            var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            storeMy.Open(OpenFlags.ReadOnly);
-            const string thumbprint = "F7DE9C384EE6D0A81DAD7E8E60BD3776FA5DE9F4";
+            //‎f7 de 9c 38 4e e6 d0 a8 1d ad 7e 8e 60 bd 37 76 fa 5d e9 f4
+            //var thumbprint = "‎‎‎84e492a972b7edc197a32d9e9c94ea27bd5ac4d9".ToUpper();
+            string thumbprint = "84e492a972b7edc197a32d9e9c94ea27bd5ac4d9".ToUpper();
 
             return CertificateUtility.SenderCertificate(thumbprint, Language.English);
         }

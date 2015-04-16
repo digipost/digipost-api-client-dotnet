d1797b01426255346306ff904b93aeb237910548
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-16 10:01:27 +0200

    Refactored Authentication level for name of constants

--- Digipost.Api.Client.Domain/AuthenticationLevel.cs
+++ Digipost.Api.Client.Domain/AuthenticationLevel.cs
@@ -4,27 +4,21 @@ using System.Xml.Serialization;
 
 namespace Digipost.Api.Client.Domain
 {
-    /// <remarks/>
+    [Serializable]
     [GeneratedCode("xsd", "4.0.30319.33440")]
-    [Serializable()]
     [XmlType(TypeName = "authentication-level", Namespace = "http://api.digipost.no/schema/v6")]
     public enum AuthenticationLevel
     {
-
-        /// <remarks/>
         [XmlEnum("PASSWORD")]
         Password,
 
-        /// <remarks/>
         [XmlEnum("TWO_FACTOR")]
         TwoFactor,
 
-        /// <remarks/>
         [XmlEnum("IDPORTEN_3")]
-        IdPorten_3,
+        IdPorten3,
 
-        /// <remarks/>
         [XmlEnum("IDPORTEN_4")]
-        IdPorten_4,
+        IdPorten4,
     }
 }

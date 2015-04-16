aa078f2fd59c09ab95fcd2dca00683c2e8efbb04
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-04-16 10:03:57 +0200

    Refactored IdentificationChoice

--- Digipost.Api.Client.Domain/IdentificationChoice.cs
+++ Digipost.Api.Client.Domain/IdentificationChoice.cs
@@ -4,23 +4,21 @@ using System.Xml.Serialization;
 
 namespace Digipost.Api.Client.Domain
 {
-    //IdentificationChoice
-    /// <remarks />
     [GeneratedCode("xsd", "4.0.30319.33440")]
     [Serializable]
     [XmlType(Namespace = "http://api.digipost.no/schema/v6", IncludeInSchema = false)]
     public enum IdentificationChoice
     {
-        /// <remarks />
-        [XmlEnum("digipost-address")] DigipostAddress,
+        [XmlEnum("digipost-address")] 
+        DigipostAddress,
 
-        /// <remarks />
-        [XmlEnum("name-and-address")] NameAndAddress,
+        [XmlEnum("name-and-address")] 
+        NameAndAddress,
 
-        /// <remarks />
-        [XmlEnum("organisation-number")] OrganisationNumber,
+        [XmlEnum("organisation-number")] 
+        OrganisationNumber,
 
-        /// <remarks />
-        [XmlEnum("personal-identification-number")] PersonalidentificationNumber
+        [XmlEnum("personal-identification-number")] 
+        PersonalidentificationNumber
     }
 }

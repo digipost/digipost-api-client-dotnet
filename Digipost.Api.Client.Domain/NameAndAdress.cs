82a83a86e69a70eb6705dec0ce454768e19ac92a
Kristian Enge <kristian.ienge@gmail.com>
2015-04-22 13:59:53 +0200

    Commented public methods in NameAndAddress.cs

--- Digipost.Api.Client.Domain/NameAndAdress.cs
+++ Digipost.Api.Client.Domain/NameAndAdress.cs
@@ -24,28 +24,51 @@ namespace Digipost.Api.Client.Domain
             PostalCode = postalCode;
             City = city;
         }
-
+        /// <summary>
+        ///     Full name of person, first name first.
+        /// </summary>
         [XmlElement("fullname")]
         public string FullName { get; set; }
 
+        /// <summary>
+        ///     Primary address
+        /// </summary>
         [XmlElement("addressline1")]
         public string AddressLine1 { get; set; }
 
+        /// <summary>
+        ///     Secondary addressline
+        /// </summary>
         [XmlElement("addressline2")]
         public string AddressLine2 { get; set; }
 
+        /// <summary>
+        ///     Postal code
+        /// </summary>
         [XmlElement("postalcode")]
         public string PostalCode { get; set; }
 
+        /// <summary>
+        ///     City
+        /// </summary>
         [XmlElement("city")]
         public string City { get; set; }
 
+        /// <summary>
+        ///     Birth date
+        /// </summary>
         [XmlElement("birth-date", DataType = "date")]
         public DateTime? BirthDate { get; set; }
 
+        /// <summary>
+        ///     Phone number
+        /// </summary>
         [XmlElement("phone-number")]
         public string PhoneNumber { get; set; }
 
+        /// <summary>
+        ///     E-mail address
+        /// </summary>
         [XmlElement("email-address")]
         public string Email { get; set; }
 

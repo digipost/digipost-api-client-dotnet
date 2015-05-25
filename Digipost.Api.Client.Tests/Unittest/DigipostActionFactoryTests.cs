03101bd51cc28ccff0a0c7c8eba41c76ebc9802e
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-05-25 15:47:23 +0200

    Refaktorert ActionFactoryTests til nytt pattern

--- Digipost.Api.Client.Tests/Unittest/DigipostActionFactoryTests.cs
+++ Digipost.Api.Client.Tests/Unittest/DigipostActionFactoryTests.cs
@@ -9,32 +9,34 @@ namespace Digipost.Api.Client.Tests.Unittest
     [TestClass]
     public class DigipostActionFactoryTests
     {
-
-        [TestMethod]
-        [ExpectedException(typeof(ConfigException), "Wrongly initialized DigipostActionFactory with wrong type.")]
-        public void DigipostActionFactory_InitializeUnknownAction_ThrowsException()
+        [TestClass]
+        public class CreateClassMethod
         {
-            var factory = new DigipostActionFactory();
-            factory.CreateClass(typeof (string), new ClientConfig(), new X509Certificate2(), "uri");
+            [TestMethod]
+            [ExpectedException(typeof(ConfigException), "Wrongly initialized DigipostActionFactory with wrong type.")]
+            public void InitializeUnknownActionThrowsException()
+            {
+                var factory = new DigipostActionFactory();
+                factory.CreateClass(typeof(string), new ClientConfig(), new X509Certificate2(), "uri");
+            }
+
+            [TestMethod]
+            public void ReturnsProperMessageAction()
+            {
+                var factory = new DigipostActionFactory();
+                var action = factory.CreateClass(typeof(Message), new ClientConfig(), new X509Certificate2(), "uri");
+
+                Assert.AreEqual(typeof(MessageAction), action.GetType());
+            }
+
+            [TestMethod]
+            public void ReturnsProperIdentificationAction()
+            {
+                var factory = new DigipostActionFactory();
+                var action = factory.CreateClass(typeof(Identification), new ClientConfig(), new X509Certificate2(), "uri");
+
+                Assert.AreEqual(typeof(IdentificationAction), action.GetType());
+            }    
         }
-
-        [TestMethod]
-        public void DigipostActionFactory_InitMessageAction_Success()
-        {
-            var factory = new DigipostActionFactory();
-            var action = factory.CreateClass(typeof(Message), new ClientConfig(), new X509Certificate2(), "uri");
-
-            Assert.AreEqual(typeof(MessageAction), action.GetType());
-        }
-
-        [TestMethod]
-        public void DigipostActionFactory_InitIdentificationAction_Success()
-        {
-            var factory = new DigipostActionFactory();
-            var action = factory.CreateClass(typeof(Identification), new ClientConfig(), new X509Certificate2(), "uri");
-
-            Assert.AreEqual(typeof(IdentificationAction), action.GetType());
-        }
-        
     }
 }

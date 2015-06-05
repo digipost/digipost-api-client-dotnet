8e368d5ab322186dd44ea5d440ecbc2c7dc484e1
Kristian Enge <kristian.ienge@gmail.com>
2015-06-05 14:49:39 +0200

    Test invoice

--- Digipost.Api.Client.Tests/Unittest/SerializeUtilTests.cs
+++ Digipost.Api.Client.Tests/Unittest/SerializeUtilTests.cs
@@ -1,4 +1,6 @@
-﻿using Digipost.Api.Client.Domain;
+﻿using System;
+using System.Globalization;
+using Digipost.Api.Client.Domain;
 using Digipost.Api.Client.Domain.Enums;
 using Digipost.Api.Client.Domain.Print;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,12 +14,12 @@ namespace Digipost.Api.Client.Tests.Unittest
         [TestClass]
         public class DeserializeMethod
         {
-            private const string MessageBlueprint = @"<?xml version=""1.0"" encoding=""utf-8""?><message xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://api.digipost.no/schema/v6""><recipient><name-and-address><fullname>Kristian Sæther Enge</fullname><addressline1>Colletts gate 68</addressline1><postalcode>0460</postalcode><city>Oslo</city></name-and-address><print-details><recipient><name>Kristian Sæther Enge</name><norwegian-address><addressline1>Collettsgate 68</addressline1><addressline2>Leil h401</addressline2><addressline3>dør 2</addressline3><zip-code>0460</zip-code><city>Oslo</city></norwegian-address></recipient><return-address><name>Ola Digipost</name><foreign-address><addressline1>svenskegatan 1</addressline1><addressline2> leil h101</addressline2><addressline3>pb 12</addressline3><addressline4>skuff 3</addressline4><country>SE</country></foreign-address></return-address><post-type>B</post-type><color>MONOCHROME</color></print-details></recipient><primary-document><uuid>1222222</uuid><subject>Subject</subject><file-type>txt</file-type><sms-notification><after-hours>2</after-hours></sms-notification><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level></primary-document><attachment><uuid>123456</uuid><subject>attachment</subject><file-type>txt</file-type><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level></attachment></message>";
+            private const string MessageBlueprint = @"<?xml version=""1.0"" encoding=""utf-8""?><message xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://api.digipost.no/schema/v6""><recipient><name-and-address><fullname>Kristian Sæther Enge</fullname><addressline1>Colletts gate 68</addressline1><postalcode>0460</postalcode><city>Oslo</city></name-and-address><print-details><recipient><name>Kristian Sæther Enge</name><norwegian-address><addressline1>Collettsgate 68</addressline1><addressline2>Leil h401</addressline2><addressline3>dør 2</addressline3><zip-code>0460</zip-code><city>Oslo</city></norwegian-address></recipient><return-address><name>Ola Digipost</name><foreign-address><addressline1>svenskegatan 1</addressline1><addressline2> leil h101</addressline2><addressline3>pb 12</addressline3><addressline4>skuff 3</addressline4><country>SE</country></foreign-address></return-address><post-type>B</post-type><color>MONOCHROME</color></print-details></recipient><primary-document><uuid>1222222</uuid><subject>Subject</subject><file-type>txt</file-type><sms-notification><after-hours>2</after-hours></sms-notification><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level></primary-document><attachment xsi:type=""invoice""><uuid>123456</uuid><subject>Subject</subject><file-type>txt</file-type><sms-notification><after-hours>2</after-hours></sms-notification><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level><kid>123123123123</kid><amount>100</amount><account>15941432384</account><due-date>2018-01-01</due-date></attachment></message>";
 
             [TestMethod]
             public void ReturnsProperDeserializedMessage()
             {
-
+                //Arrange
                 var printDetails = new PrintDetails(new PrintRecipient("Kristian Sæther Enge", new NorwegianAddress("0460", "Oslo", "Collettsgate 68", "Leil h401", "dør 2")), new PrintReturnAddress("Ola Digipost", new ForeignAddress(CountryIdentifier.Country, "SE", "svenskegatan 1", " leil h101", "pb 12", "skuff 3")));
                 var recipient =
                     new Recipient(
@@ -25,20 +27,21 @@ namespace Digipost.Api.Client.Tests.Unittest
                 var document = new Document("Subject", "txt", TestHelper.GetBytes("test"), AuthenticationLevel.TwoFactor,
                     SensitivityLevel.Sensitive) { Guid = "1222222", SmsNotification = new SmsNotification(2) };
 
-                var attachment = new Document("attachment", "txt", TestHelper.GetBytes("test"), AuthenticationLevel.TwoFactor,
-                    SensitivityLevel.Sensitive) { Guid = "123456" };
+                var attachment =  new Invoice("Subject", "txt", TestHelper.GetBytes("test"),100,"15941432384",DateTime.Parse("2018-01-01"),"123123123123", AuthenticationLevel.TwoFactor,
+                    SensitivityLevel.Sensitive) { Guid = "123456", SmsNotification = new SmsNotification(2) };
 
 
                 var message = new Message(recipient, document);
                 message.Attachments.Add(attachment);
 
+                //Act
                 var serialized = SerializeUtil.Serialize(message);
                 Assert.IsNotNull(serialized);
-
                 var deserializedMessage = SerializeUtil.Deserialize<Message>(serialized);
 
                 var deserializedMessageBlueprint = SerializeUtil.Deserialize<Message>(MessageBlueprint);
 
+                //Assert
                 TestHelper.LookLikeEachOther(deserializedMessageBlueprint, deserializedMessage);
             }
             
@@ -47,6 +50,24 @@ namespace Digipost.Api.Client.Tests.Unittest
         [TestClass]
         public class SerializeMethod
         {
+            private const string InvoiceBlueprint = @"<?xml version=""1.0"" encoding=""utf-8""?><invoice xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://api.digipost.no/schema/v6""><uuid>123456</uuid><subject>Subject</subject><file-type>txt</file-type><sms-notification><after-hours>2</after-hours></sms-notification><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level><kid>123123123123</kid><amount>100</amount><account>15941432384</account><due-date>2018-01-01</due-date></invoice>";
+
+            [TestMethod]
+            public void ReturnProperSerializedInvoice()
+            {
+                //Arrange
+                var invoice = new Invoice("Subject", "txt", TestHelper.GetBytes("test"),100,"15941432384",DateTime.Parse("2018-01-01"),"123123123123", AuthenticationLevel.TwoFactor,
+                    SensitivityLevel.Sensitive) { Guid = "123456", SmsNotification = new SmsNotification(2) };
+
+                //Act
+                var serialized = SerializeUtil.Serialize(invoice);
+
+                //Assert
+                Assert.IsNotNull(serialized);
+                Assert.AreEqual(InvoiceBlueprint, serialized);
+
+            }
+
             private const string DocumentBlueprint = @"<?xml version=""1.0"" encoding=""utf-8""?><document xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns=""http://api.digipost.no/schema/v6""><uuid>123456</uuid><subject>Subject</subject><file-type>txt</file-type><sms-notification><after-hours>2</after-hours></sms-notification><authentication-level>TWO_FACTOR</authentication-level><sensitivity-level>SENSITIVE</sensitivity-level></document>";
             
             [TestMethod]

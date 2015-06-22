c35009825c756c2a2a2e39202730fadbe5d85ab7
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-06-22 13:49:33 +0200

    Signering lagt tilpå assemblies, tester kjører ikke

--- Digipost.Api.Client/Properties/AssemblyInfo.cs
+++ Digipost.Api.Client/Properties/AssemblyInfo.cs
@@ -14,8 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Digipost ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Digipost.Api.Client.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: InternalsVisibleTo("Digipost.Api.Client.Tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100cf139fbd5e0eab562789ebec8bb2d8b3d2d85fd49149bb0d78b1d9a614ab55e42987af50c0ca26c11abbf41d6aa8f737ceac1a65aff272565f14c4ab9c02d4018ccaa06d99e9b790f79c8b66d0f05952c36014bfe511ea866d47509abbc93ee3e37bc830520d14e70a18536084b544da95f4a6fa02c0382e968d00c5768761ad")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2,PublicKey=0024000004800000940000000602000000240000525341310004000001000100cf139fbd5e0eab562789ebec8bb2d8b3d2d85fd49149bb0d78b1d9a614ab55e42987af50c0ca26c11abbf41d6aa8f737ceac1a65aff272565f14c4ab9c02d4018ccaa06d99e9b790f79c8b66d0f05952c36014bfe511ea866d47509abbc93ee3e37bc830520d14e70a18536084b544da95f4a6fa02c0382e968d00c5768761ad")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

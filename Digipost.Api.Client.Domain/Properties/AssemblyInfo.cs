00c9bc4e75921a172f07f8aba01b18a433fbe99e
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-06-26 09:30:27 +0200

    Endret til strong-name-nøkkel delt for alle prosjekter

--- Digipost.Api.Client.Domain/Properties/AssemblyInfo.cs
+++ Digipost.Api.Client.Domain/Properties/AssemblyInfo.cs
@@ -14,15 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-<<<<<<< Updated upstream
-[assembly: InternalsVisibleTo("Digipost.Api.Client")]
-[assembly: InternalsVisibleTo("Digipost.Api.Client.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-=======
 [assembly: InternalsVisibleTo("Digipost.Api.Client,PublicKey=00240000048000009400000006020000002400005253413100040000010001008b3388f9c416425f0145bbcf26e66b9a87c4e08b4cd41563e4bc8846df38ba4d997c5408cc77da26d79b03c39874a6af9df0aff3e7bdb3c0e53a91f6d19c50e160f5bf67986a04f0f985eca0252f557ed9ae520dd51e3107d6168d073d4ec5ada28d34e492ad9fb7af29c82309c5c0124211e679caea38d5463d2af9042dafda")]
 [assembly: InternalsVisibleTo("Digipost.Api.Client.Tests,PublicKey=00240000048000009400000006020000002400005253413100040000010001008b3388f9c416425f0145bbcf26e66b9a87c4e08b4cd41563e4bc8846df38ba4d997c5408cc77da26d79b03c39874a6af9df0aff3e7bdb3c0e53a91f6d19c50e160f5bf67986a04f0f985eca0252f557ed9ae520dd51e3107d6168d073d4ec5ada28d34e492ad9fb7af29c82309c5c0124211e679caea38d5463d2af9042dafda")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2,PublicKey=0024000004800000940000000602000000240000525341310004000001000100cf139fbd5e0eab562789ebec8bb2d8b3d2d85fd49149bb0d78b1d9a614ab55e42987af50c0ca26c11abbf41d6aa8f737ceac1a65aff272565f14c4ab9c02d4018ccaa06d99e9b790f79c8b66d0f05952c36014bfe511ea866d47509abbc93ee3e37bc830520d14e70a18536084b544da95f4a6fa02c0382e968d00c5768761ad")]
->>>>>>> Stashed changes
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

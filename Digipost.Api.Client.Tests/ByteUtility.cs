1b345a4f2ce499a03bf6d11609aaa540b6391016
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-07-24 15:02:21 +0200

    Separer TestHelper i ByteUtility og Comparator

---
+++ Digipost.Api.Client.Tests/ByteUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Digipost.Api.Client.Tests
+{
+    internal class ByteUtility
+    {
+        public static byte[] GetBytes(string str)
+        {
+            var bytes = new byte[str.Length * sizeof(char)];
+            Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+    }
+}

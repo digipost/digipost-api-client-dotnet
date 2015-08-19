a13642b71a9ac6654c12d3d6d656fd6031e0666d
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-08-19 10:25:27 +0200

    Lag tester som blir godkjent for Identification

--- Digipost.Api.Client.Tests/Unittest/DtoTests/IdentificationTests.cs
+++ Digipost.Api.Client.Tests/Unittest/DtoTests/IdentificationTests.cs
@@ -1,49 +1,98 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Digipost.Api.Client.Domain;
+using Digipost.Api.Client.Domain.Enums;
+using Digipost.Api.Client.Domain.Identification;
+using Digipost.Api.Client.Tests.CompareObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Digipost.Api.Client.Tests.Unittest.DtoTests
 {
     [TestClass]
     public class IdentificationTests
     {
+        public IComparator Comparator = new Comparator();
+
         [TestClass]
-        public class ConstructorMethod
+        public class ConstructorMethod : IdentificationTests
         {
             [TestMethod]
             public void InitializeProperlyForStringInitialization()
             {
-                Assert.Fail();   
+                //Arrange
+                Identification identification = new Identification(IdentificationChoice.DigipostAddress, "Ola.nordmann#234HH");
+                IdentificationDto identificationDto = new IdentificationDto(IdentificationChoice.DigipostAddress, "Ola.nordmann#234HH");
+                
+                //Act
+
+                //Assert
+                IEnumerable<IDifference> differences;
+                Comparator.AreEqual(identificationDto, identification.DataTransferObject, out differences);
+                Assert.AreEqual(0, differences.Count());
             }
 
             [TestMethod]
             public void InitializeProperlyForRecipientInitialization()
             {
-                Assert.Fail();
+                //Arrange
+                Identification identification = new Identification(new RecipientByNameAndAddress("Ola Nordmann", "0001", "Oslo", "Gateveien 2"));
+                IdentificationDto identificationDto = new IdentificationDto(new RecipientByNameAndAddress("Ola Nordmann", "0001", "Oslo", "Gateveien 2"));
+
+                //Act
+
+                //Assert
+                IEnumerable<IDifference> differences;
+                Comparator.AreEqual(identificationDto, identification.DataTransferObject, out differences);
+                Assert.AreEqual(0, differences.Count());
             }
         }
 
         [TestClass]
-        public class IdentificationValueField
+        public class IdentificationValueField : IdentificationTests
+        
         {
             [TestMethod]
             public void ReturnsCorrectIdentificationValueForStringInitialization()
             {
-                Assert.Fail();
+                //Arrange
+                Identification identification = new Identification(IdentificationChoice.DigipostAddress, "Ola.nordmann#234HH");
+
+                //Act
+
+                //Assert
+                Assert.AreEqual("Ola.nordmann#234HH", (string)identification.IdentificationValue);
             }
 
             [TestMethod]
             public void ReturnsCorrectIdentificationValueForRecipientInitialization()
             {
-                Assert.Fail();
+                //Arrange
+                var recipient = new RecipientByNameAndAddress("Ola Nordmann", "0001", "Oslo", "Gateveien 2");
+                Identification identification = new Identification(new RecipientByNameAndAddress("Ola Nordmann", "0001", "Oslo", "Gateveien 2"));
+
+                //Act
+
+                //Assert
+                IEnumerable<IDifference> differences;
+                Comparator.AreEqual(recipient, identification.IdentificationValue, out differences);
+                Assert.AreEqual(0, differences.Count());
             }
         }
 
         [TestClass]
-        public class IdentificationTypeField
+        public class IdentificationTypeField : IdentificationTests
         {
             [TestMethod]
-            public void ReturnsCorrectIdentificationType()
+            public void ReturnsCorrectIdentificationType() 
             {
-                Assert.Fail();
+                //Arrange
+                Identification identification = new Identification(IdentificationChoice.DigipostAddress, "Ola.nordmann#234HH");
+
+                //Act
+
+                //Assert
+                Assert.AreEqual(IdentificationChoice.DigipostAddress, identification.IdentificationType);
             }
         }
 

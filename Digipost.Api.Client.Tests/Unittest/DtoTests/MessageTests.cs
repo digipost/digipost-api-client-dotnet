83ed2e7d92e43d995b657f6c72dd62a638d55c9d
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-08-25 13:46:16 +0200

    Lag heldekkende tester for Message

--- Digipost.Api.Client.Tests/Unittest/DtoTests/MessageTests.cs
+++ Digipost.Api.Client.Tests/Unittest/DtoTests/MessageTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Digipost.Api.Client.Domain.Enums;
+using Digipost.Api.Client.Domain.SendMessage;
+using Digipost.Api.Client.Tests.Integration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Digipost.Api.Client.Tests.Unittest.DtoTests
 {
@@ -9,17 +13,75 @@ namespace Digipost.Api.Client.Tests.Unittest.DtoTests
         public class ConstructorMethod : MessageTests
         {
             [TestMethod]
-            public void RecipientAndDocuments()
+            public void ConstructWithRecipientAndPrimaryDocument()
             {
                 //Arrange
-                                
+                Message message = new Message(
+                    recipient: DomainUtility.GetRecipientWithDigipostId(), 
+                    primaryDocument: DomainUtility.GetDocument()
+                    );                
 
                 //Act
 
                 //Assert
-            } 
+                Assert.IsNotNull(message.PrimaryDocument);
+                Assert.IsNull(message.SenderId);
+            }
+
+            [TestMethod]
+            public void ConstructWithSenderId()
+            {
+                //Arrange
+                Message message = new Message(
+                    recipient: DomainUtility.GetRecipientWithDigipostId(),
+                    primaryDocument: DomainUtility.GetDocument(),
+                    senderId: "123456"
+                );                
+                
+                //Act
+
+                //Assert
+                Assert.IsNotNull(message.PrimaryDocument);
+                Assert.IsNotNull(message.SenderId);
+                Assert.IsNotNull(message.Attachments);
+                Assert.AreEqual("123456",message.SenderId);
+            }
         }
 
+        [TestClass]
+        public class DeliveryTimeSpecifiedMethod : MessageTests
+        {
+            [TestMethod]
+            public void DeliveryTimeNotSpecifiedGivesFalse()
+            {
+                //Arrange
+                Message message = new Message(
+                    recipient: DomainUtility.GetRecipientWithDigipostId(),
+                    primaryDocument: DomainUtility.GetDocument()
+                    );               
+                
+                //Act
 
+                //Assert
+                Assert.IsFalse(message.DeliveryTimeSpecified);
+            }
+
+            [TestMethod]
+            public void DeliveryTimeSpecifiedGivesTrue()
+            {
+                //Arrange
+                Message message = new Message(
+                    recipient: DomainUtility.GetRecipientWithDigipostId(),
+                    primaryDocument: DomainUtility.GetDocument()
+                    );
+                message.DeliveryTime = DateTime.Today;
+
+                //Act
+
+                //Assert
+                Assert.IsTrue(message.DeliveryTimeSpecified);
+            }
+
+        }
     }
 }

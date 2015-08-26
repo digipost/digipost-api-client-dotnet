88d52c190c8ad0ff8c31c5cec3905aa9da8a2ab1
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-08-26 10:31:01 +0200

    Fjern overflødig konstruktør for InvoiceDTO

--- Digipost.Api.Client.Domain/SendMessage/InvoiceDataTransferObject.cs
+++ Digipost.Api.Client.Domain/SendMessage/InvoiceDataTransferObject.cs
@@ -11,17 +11,7 @@ namespace Digipost.Api.Client.Domain
     [XmlRoot(Namespace = "http://api.digipost.no/schema/v6", IsNullable = false)]
     public class InvoiceDataTransferObject : DocumentDataTransferObject
     {
-        public InvoiceDataTransferObject(string subject, string fileType, string path, decimal amount, string account, DateTime duedate, string kid= null,
-            AuthenticationLevel authLevel = AuthenticationLevel.Password,
-            SensitivityLevel sensitivityLevel = SensitivityLevel.Normal) : base(subject,fileType,path,authLevel,sensitivityLevel)
-        {
-            Kid = kid;
-            Amount = amount;
-            Account = account;
-            Duedate = duedate;
-        }
-
-        public InvoiceDataTransferObject(string subject, string fileType, byte[] contentBytes, decimal amount, string account, DateTime duedate, string kid= null,
+      public InvoiceDataTransferObject(string subject, string fileType, byte[] contentBytes, decimal amount, string account, DateTime duedate, string kid= null,
             AuthenticationLevel authLevel = AuthenticationLevel.Password,
             SensitivityLevel sensitivityLevel = SensitivityLevel.Normal, SmsNotification smsNotification = null): base(subject,fileType,contentBytes,authLevel,sensitivityLevel)
         {

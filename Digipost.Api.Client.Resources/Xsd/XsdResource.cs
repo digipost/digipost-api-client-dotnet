35280c88c86ad8a57df7e1a08995b013e55b9d93
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-09-04 20:18:00 +0200

    Adds auto-generated DTO classes
    This commit does the following:
    - Adds a Resource project that is to be used for resources in project, reducing duplication of test data files.
    - Adds a Scripts project adding a shell script to create classes from XSD together with the xsd and auto-generated DTO classes. Not in use at the following point.

---
+++ Digipost.Api.Client.Resources/Xsd/XsdResource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using ApiClientShared;
+
+namespace Digipost.Api.Client.Resources.Xsd
+{
+    internal static class XsdResource
+    {
+        private static readonly ResourceUtility ResourceUtility = new ResourceUtility("Digipost.Api.Client.Resources.Xsd.Data");
+
+        private static XmlReader GetResource(params string[] path)
+        {
+            var bytes = ResourceUtility.ReadAllBytes(true, path);
+            return XmlReader.Create(new MemoryStream(bytes));
+        }
+
+        public static XmlReader GetApiV6Xsd()
+        {
+            return GetResource("api_v6.xsd");
+        }
+    }
+}
---
+++ Digipost.Api.Client.Scripts/XsdToCode/GenerateClassesFromXs.ps1
@@ -0,0 +1,36 @@
+﻿# RUNNING THE FIRST TIME
+# To be able to run the first time, enable running of scripts for current user. Open PowerShell as admin and run:
+#	Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
+# If you want to check that it was set:
+#	Get-ExecutionPolicy -List | Format-Table -AutoSize
+# Set policy to restricted afterwards:
+#  Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy Restricted
+# If problems running script from network drive:
+#  net use Z: `"\\vmware-host\Shared Folders`
+
+function GenerateCode($XsdPath, $OutDir)
+{
+    Write-Host("XSD: $XsdPath")
+    Write-Host("OUT: $OutDir")
+    
+    $CurrentDirectory = (Get-Item -Path ".\" -Verbose).FullName
+	Set-Location "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\"
+    Invoke-Expression ".\Xsd.exe $XsdPath /classes /out:$OutDir"
+    Set-Location $CurrentDirectory
+}
+
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
+$CurrentScriptDir = Get-ScriptDirectory
+
+$Xsd1 = "\Xsd\api_v7.xsd"
+$XsdPath = "$CurrentScriptDir$Xsd1"
+
+$XsdToCodeDir = (Get-Item $CurrentScriptDir)
+$CodeDir =  "$XsdToCodeDir\Code"
+
+GenerateCode $XsdPath "$CodeDir"

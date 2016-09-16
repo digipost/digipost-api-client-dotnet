caeec644820c1a778e6f67f84f364c8bda50ee33
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-09-16 15:26:46 +0200

    Adds NorskHelsenett as Environment endpoint

--- Digipost.Api.Client/Environment.cs
+++ Digipost.Api.Client/Environment.cs
@@ -13,12 +13,15 @@ namespace Digipost.Api.Client
 
         public static Environment Production => new Environment(new Uri("https://api.digipost.no/"));
 
+        public static Environment NorskHelsenett => new Environment(new Uri("https://api.nhn.digipost.no"));
+
         public static Environment Test => new Environment(new Uri("https://api.test.digipost.no/"));
 
         public static Environment Qa => new Environment(new Uri("https://qa.api.digipost.no/"));
 
         public static Environment Preprod => new Environment(new Uri("https://qaoffentlig.api.digipost.no/"));
 
+        
         public override string ToString()
         {
             return $"Url: {Url}";

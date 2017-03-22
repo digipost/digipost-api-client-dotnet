79f751cdb43628c66cab85a47ca40a4fa88bccbe
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-03-22 09:39:34 +0100

    Lager eget prosjekt for common-tester

--- Digipost.Api.Client.Common.Tests/DigipostActionTests.cs
+++ Digipost.Api.Client.Common.Tests/DigipostActionTests.cs
@@ -34,21 +34,6 @@ namespace Digipost.Api.Client.Tests.Action
                 var expected = SerializeUtil.Serialize(identificationDto);
                 Assert.Equal(expected, content.InnerXml);
             }
-
-            [Fact]
-            public void ReturnsCorrectDataForMessage()
-            {
-                //Arrange
-                var message = DomainUtility.GetSimpleMessageWithRecipientById();
-
-                //Act
-                var action = new MessageAction(message);
-                var content = action.RequestContent;
-
-                //Assert
-                var expected = SerializeUtil.Serialize(SendDataTransferObjectConverter.ToDataTransferObject(message));
-                Assert.Equal(expected, content.InnerXml);
-            }
         }
     }
 }

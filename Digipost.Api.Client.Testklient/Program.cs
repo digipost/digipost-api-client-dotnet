d3aee993297ac592661e3811ff01167d5e76292c
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-03-24 12:37:55 +0100

    Legger inn avhengighet til biblioteket i Testclient
    Fordi vi trenger å teste at den eksterne avhengigheten fungerer, og koden som var der fra før var død. Det er ofte at vi glemmer å legge ved en .dll eller har et prosjekt som på en annen måte er ugyldig.

--- Digipost.Api.Client.Testklient/Program.cs
+++ Digipost.Api.Client.Testklient/Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Reflection;
-using Common.Logging;
 using Digipost.Api.Client.Common;
 using Digipost.Api.Client.Common.Enums;
-using Digipost.Api.Client.Common.Exceptions;
-using Digipost.Api.Client.Common.Identify;
-using Digipost.Api.Client.Common.Print;
 using Digipost.Api.Client.Common.Recipient;
-using Digipost.Api.Client.Common.Search;
-using Digipost.Api.Client.ConcurrencyTest;
-using Digipost.Api.Client.Resources.Content;
 using Digipost.Api.Client.Send;
 using Environment = Digipost.Api.Client.Common.Environment;
 
@@ -17,162 +9,21 @@ namespace Digipost.Api.Client.Testklient
 {
     internal class Program
     {
-        private const string Thumbprint = "d8 6e 19 1b 8f 9b 0b 57 3e db 72 db a8 09 1f dc 6a 10 18 fd";
-        private const long SenderId = 779051;
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Log.Debug("Starting console application ...");
-            RunSingle();
-            Console.ReadKey();
-        }
+            var environment = Environment.Production;
+            environment.Url = new Uri("https://api.qa.digipost.no");
 
-        private static void RunSingle()
-        {
-            var config = new ClientConfig(new Broker(SenderId), Environment.Production);
+            var config = new ClientConfig(new Broker(1010), environment);
+            var client = new DigipostClient(config, thumbprint: "2d 7f 30 dd 05 d3 b7 fc 7a e5 97 3a 73 f8 49 08 3b 20 40 ed");
 
-            //Logging.Initialize(config);
-            var api = new DigipostClient(config, Thumbprint);
+            var message = new Message(
+                new Sender(10100),
+                new RecipientById(IdentificationType.PersonalIdentificationNumber, "01043100358"),
+                new Document(subject: "Attachment", fileType: "txt", path: @"\\vmware-host\Shared Folders\Downloads\samlple.xml")
+            );
 
-            IdentifyPerson(api);
-            SendMessageToPerson(api);
-            //var response = Search(api);
-
-            //var res = api.GetPersonDetails(response.AutcompleteSuggestions[0]);
-            //ConcurrencyTest.Initializer.Run(); //concurency runner
-
-            Console.ReadKey();
-        }
-
-        private static void Performance()
-        {
-            Initializer.Run(); //concurency runner
-        }
-
-        private static ISearchDetailsResult Search(DigipostClient api)
-        {
-            return api.Search("Al");
-        }
-
-        private static async void SendMessageToPerson(DigipostClient api, bool isQaOrLocal = false)
-        {
-            Console.WriteLine("======================================");
-            Console.WriteLine("Sending message:");
-            Console.WriteLine("======================================");
-
-            var message = isQaOrLocal ? GetMessageWithRecipientByIdForQaOrLocal() : GetMessage();
-
-            try
-            {
-                var messageDeliveryResult = await api.SendMessageAsync(message).ConfigureAwait(false);
-                Console.WriteLine("> Starter å sende melding");
-                WriteToConsoleWithColor("Meldingens status: " + messageDeliveryResult.Status);
-                WriteToConsoleWithColor("> Alt gikk fint!");
-            }
-            catch (AggregateException ae)
-            {
-                ae.Handle(x =>
-                {
-                    if (x is ClientResponseException)
-                    {
-                        Console.WriteLine("This really failed!");
-                        return true;
-                    }
-                    return false;
-                });
-            }
-            catch (ClientResponseException e)
-            {
-                var errorMessage = e.Error;
-                WriteToConsoleWithColor("> Error." + errorMessage, true);
-            }
-            catch (Exception e)
-            {
-                WriteToConsoleWithColor("> Oh snap... " + e.Message + e.InnerException?.Message, true);
-            }
-        }
-
-        private static void IdentifyPerson(DigipostClient api)
-        {
-            Console.WriteLine("======================================");
-            Console.WriteLine("Identifiserer person:");
-            Console.WriteLine("======================================");
-            var identification = new Identification(new RecipientById(IdentificationType.OrganizationNumber, "896295291"));
-
-            try
-            {
-                var identificationResponse = api.Identify(identification);
-                Log.Debug("Identification resp: \n" + identificationResponse);
-                WriteToConsoleWithColor("> Personen ble identifisert!");
-
-                Console.WriteLine("ResultType: " + identificationResponse.ResultType);
-                Console.WriteLine("IdentificationValue: " + identificationResponse.Data);
-                Console.WriteLine("Error: " + identificationResponse.Error);
-            }
-            catch (ClientResponseException e)
-            {
-                var errorMessage = e.Error;
-                WriteToConsoleWithColor("> Feilet." + errorMessage, true);
-            }
-            catch (Exception e)
-            {
-                WriteToConsoleWithColor("> Oh snap... " + e.Message, true);
-            }
-        }
-
-        private static IMessage GetMessageWithRecipientByIdForQaOrLocal()
-        {
-            //primary document
-            var primaryDocument = new Document("Primary document", "txt", GetPrimaryDocument());
-
-            var digitalRecipient = new RecipientById(IdentificationType.PersonalIdentificationNumber, "01013300001");
-
-            var message = new Message(new Sender(1010), digitalRecipient, primaryDocument);
-
-            return message;
-        }
-
-        private static IMessage GetMessage()
-        {
-            //primary document
-            var primaryDocument = new Document("Primary document", "txt", GetPrimaryDocument());
-            var invoice = new Invoice("Invoice 1", "txt", GetPrimaryDocument(), 1, "18941362738", DateTime.Now, "123123123");
-
-            //attachment
-            var attachment = new Document("Attachment", "txt", GetAttachment());
-
-            //printdetails for fallback to print (physical mail)
-            var printDetails =
-                new PrintDetails(
-                    new PrintRecipient("Kristian Sæther Enge", new NorwegianAddress("0460", "Oslo", "Colletts gate 68")),
-                    new PrintReturnRecipient("Kristian Sæther Enge",
-                        new NorwegianAddress("0460", "Oslo", "Colletts gate 68"))
-                );
-
-            //message
-            var message = new Message(new Sender(1010), new RecipientById(IdentificationType.PersonalIdentificationNumber, "07068932715"), invoice);
-
-            message.Attachments.Add(attachment);
-
-            return message;
-        }
-
-        private static byte[] GetPrimaryDocument()
-        {
-            return ContentResource.Hoveddokument.PlainText();
-        }
-
-        private static byte[] GetAttachment()
-        {
-            return ContentResource.Vedlegg.Text();
-        }
-
-        private static void WriteToConsoleWithColor(string message, bool isError = false)
-        {
-            Console.ForegroundColor = isError ? ConsoleColor.Red : ConsoleColor.Green;
-            Console.WriteLine(message);
-            Console.ResetColor();
+            var result = client.SendMessage(message);
         }
     }
 }

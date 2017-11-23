7e4c60f49c5f5790352fe91b44f1ae519e368dee
Sindre Nordbø <sindre.nordbo@bekk.no>
2017-11-23 09:50:42 +0100

    Dokumentert restriksjoner på maks lengde
    Dette blir håndhevet på back-end, men for å gi et hint til konsumenter
    av klientbiblioteket dokumenteres det her.

--- Digipost.Api.Client.DataTypes/Appointment/AppointmentAddress.cs
+++ Digipost.Api.Client.DataTypes/Appointment/AppointmentAddress.cs
@@ -14,10 +14,19 @@
             City = city;
         }
 
+        /// <summary>
+        ///     Optional street address. 100 characters or less.
+        /// </summary>
         public string StreetAddress { get; set; }
 
+        /// <summary>
+        ///     10 characters or less.
+        /// </summary>
         public string PostalCode { get; set; }
 
+        /// <summary>
+        ///     100 characters or less.
+        /// </summary>
         public string City { get; set; }
 
         internal appointmentAddress AsDataTransferObject()

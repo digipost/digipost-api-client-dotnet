404333a191e0e78b1f10c7d094c09ff59709c1e6
AaronArrick <arrickaaron@gmail.com>
2019-07-30 11:54:51 +0200

    Legg til ny script for generering av kode fra XSD.

---
+++ Digipost.Api.Client.Scripts/XsdToCode/GenerateClassesFromXsd.ps1
@@ -0,0 +1,34 @@
+# Running for the first time:
+# Install Powershell with command ...
+# brew cask install powershell
+
+
+function GenerateCode($XsdDirectory, $RootXsd, $OutDir)
+{
+    Write-Host("[Generating code from XSD: $XsdDirectory]")
+    Write-Host("Output-directory: $OutDir")
+    Write-Host("Changing directory to $XsdDirectory")
+    
+    Set-Location $XsdDirectory
+
+    Invoke-Expression "xsd $RootXsd /classes /outputdir:$OutDir"
+}
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$CurrentScriptDir = Get-ScriptDirectory
+$SourceDir = (Get-Item $CurrentScriptDir).Parent.Parent.FullName
+$XsdToCodeDir = (Get-Item $CurrentScriptDir)
+$CurrentApiVersion = "v7"
+
+$CodeDir = "$XsdToCodeDir/Code"
+$XsdPath = "$SourceDir/Digipost.Api.Client.Resources/Xsd/Data"
+$RootDatatypesXsd = "$XsdPath/datatypes.xsd"
+$RootApiXsd = "$XsdPath/api_$CurrentApiVersion.xsd"
+
+GenerateCode "$XsdPath" "$RootDatatypesXsd" "$CodeDir"
+GenerateCode "$XsdPath" "$RootApiXsd" "$CodeDir"

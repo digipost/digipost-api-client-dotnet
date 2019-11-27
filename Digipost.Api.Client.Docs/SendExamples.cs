dc02f7447fcc1d05cdc7e183ad642f2f9c3bf6e7
AaronArrick <arrickaaron@gmail.com>
2019-11-27 13:01:06 +0100

    Change a bit of examples

--- Digipost.Api.Client.Docs/SendExamples.cs
+++ Digipost.Api.Client.Docs/SendExamples.cs
@@ -15,8 +15,7 @@ namespace Digipost.Api.Client.Docs
 {
     public class SendExamples
     {
-        private static readonly ClientConfig ClientConfig = new ClientConfig(new Broker(123456), Environment.Production);
-        private static readonly DigipostClient client = new DigipostClient(ClientConfig, thumbprint: "84e492a972b7e...");
+        private static readonly DigipostClient client;
         private static readonly Sender sender = new Sender(67890);
 
         public void ClientConfigurationWithThumbprint()
@@ -31,13 +30,16 @@ namespace Digipost.Api.Client.Docs
 
         public void ClientConfigurationWithCertificate()
         {
-            // The actual sender of the message. The broker is the owner of the organization certificate 
+            // The actual sender of the message. The broker is the owner of the organization certificate
             // used in the library. The broker id can be retrieved from your Digipost organization account.
-            const int brokerId = 12345;
-            
-            var broker = new Broker(brokerId);
+            var broker = new Broker(12345);
+
+            // The sender is what the receiver of the message sees as the sender of the message.
+            // Sender and broker id will both be your organization's id if you are sending on behalf of yourself.
+            var sender = new Sender(67890);
+
             var clientConfig = new ClientConfig(broker, Environment.Production);
-            
+
             var client = new DigipostClient(clientConfig, CertificateReader.ReadCertificate());
         }
 
--- docs/_v10_0/2_send_messages.md
+++ docs/_v10_0/2_send_messages.md
@@ -9,11 +9,14 @@ layout: default
 `ClientConfig` is a container for all the connection specific parameters that you can set.
 
 ```csharp
-// The actual sender of the message. The broker is the owner of the organization certificate 
+// The actual sender of the message. The broker is the owner of the organization certificate
 // used in the library. The broker id can be retrieved from your Digipost organization account.
-const int brokerId = 12345;
-            
-var broker = new Broker(brokerId);
+var broker = new Broker(12345);
+
+// The sender is what the receiver of the message sees as the sender of the message.
+// Sender and broker id will both be your organization's id if you are sending on behalf of yourself.
+var sender = new Sender(67890);
+
 var clientConfig = new ClientConfig(broker, Environment.Production);
 
 var client = new DigipostClient(clientConfig, CertificateReader.ReadCertificate());

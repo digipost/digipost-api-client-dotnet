e6e79425ccbca1881f9f74d96a7fb4d286c8f1d8
sindremb <sindre.mb@gmail.com>
2020-01-30 10:05:28 +0100

    Changed Document-class' conversion of path and stream to bytes to be identical to Invoice-class' implementation to resolve issue where the constructor taking a stream as input erroneously expects the stream to contain the file path to a document and not the actual document contents. (#167)
    Updated test accordingly. Removed Fakes of Invoice and Document, as these are no longer needed.

--- Digipost.Api.Client.Send.Tests/DocumentTests.cs
+++ Digipost.Api.Client.Send.Tests/DocumentTests.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using Digipost.Api.Client.Common;
 using Digipost.Api.Client.Common.Enums;
-using Digipost.Api.Client.Send.Tests.Fakes;
+using Digipost.Api.Client.Resources.Content;
 using Xunit;
 
 namespace Digipost.Api.Client.Send.Tests
@@ -30,18 +30,20 @@ namespace Digipost.Api.Client.Send.Tests
             }
 
             [Fact]
-            public void DocumentFromFile()
+            public void DocumentFromFilePath()
             {
                 //Arrange
+                var tempFile = CreateTempFile();
 
                 //Act
-                var document = new FakeDocument("Subject", "txt", "c://imaginary/file", AuthenticationLevel.TwoFactor, SensitivityLevel.Sensitive, new SmsNotification(2));
+                var document = new Document("Subject", "txt", tempFile, AuthenticationLevel.TwoFactor, SensitivityLevel.Sensitive, new SmsNotification(2));
 
                 //Assert
                 Assert.NotNull(document.Guid);
                 Assert.Equal("Subject", document.Subject);
                 Assert.Equal("txt", document.FileType);
-                Assert.True(document.ContentBytes.SequenceEqual(new byte[] {1, 2, 3, 4}));
+                var expectedBytes = BytesFromStream(new FileStream(tempFile, FileMode.Open));
+                Assert.Equal(expectedBytes, document.ContentBytes);
                 Assert.Equal(AuthenticationLevel.TwoFactor, document.AuthenticationLevel);
                 Assert.Equal(SensitivityLevel.Sensitive, document.SensitivityLevel);
                 //Assert.Equal(new SmsNotification(2), document.SmsNotification);
@@ -51,20 +53,43 @@ namespace Digipost.Api.Client.Send.Tests
             public void DocumentFromStream()
             {
                 //Arrange
-                byte[] myByteArray = {1, 2, 3};
-                var stream = new MemoryStream(myByteArray);
+                var contentStream = new MemoryStream(ContentResource.Hoveddokument.Pdf());
 
                 //Act
-                Document document = new FakeDocument("Subject", "txt", stream, AuthenticationLevel.TwoFactor, SensitivityLevel.Sensitive, new SmsNotification(2));
+                Document document = new Document("Subject", "txt", contentStream, AuthenticationLevel.TwoFactor, SensitivityLevel.Sensitive, new SmsNotification(2));
 
                 //Assert
                 Assert.NotNull(document.Guid);
                 Assert.Equal("Subject", document.Subject);
                 Assert.Equal("txt", document.FileType);
-                Assert.True(document.ContentBytes.SequenceEqual(new byte[] {1, 2, 3}));
+                var expectedBytes = BytesFromStream(new MemoryStream(ContentResource.Hoveddokument.Pdf()));
+                Assert.Equal(expectedBytes, document.ContentBytes);
                 Assert.Equal(AuthenticationLevel.TwoFactor, document.AuthenticationLevel);
                 Assert.Equal(SensitivityLevel.Sensitive, document.SensitivityLevel);
                 //Assert.Equal(new SmsNotification(2), document.SmsNotification);
+            }
+
+            private static string CreateTempFile()
+            {
+                var tempFile = Path.GetTempFileName();
+                using (var fileStream = new FileStream(tempFile, FileMode.Append))
+                {
+                    using (var writer = new StreamWriter(fileStream))
+                    {
+                        writer.WriteLine("testulf testesen");
+                        writer.Flush();
+                        writer.Close();
+                    }
+                }
+                return tempFile;
+            }
+
+            private static byte[] BytesFromStream(Stream fileStream)
+            {
+                if (fileStream == null) return null;
+                var bytes = new byte[fileStream.Length];
+                fileStream.Read(bytes, 0, bytes.Length);
+                return bytes;
             }
         }
     }

3fcee6632251df17af8f7793e8627b62ac51e558
Arne Røen <arne.roen@posten.no>
2026-09-15 17:09:00 +0200

    fiks feil i useragent

--- Digipost.Api.Client/Internal/RequestHeaderUtility.cs
+++ Digipost.Api.Client/Internal/RequestHeaderUtility.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Reflection;
-using System.Runtime;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Digipost.Api.Client.Common;
 using Org.BouncyCastle.Crypto;
@@ -62,28 +62,7 @@ namespace Digipost.Api.Client.Internal
         {
             var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
 
-            return $"digipost-api-client-dotnet/{assemblyVersion} (netcore/{GetNetCoreVersion()})";
-        }
-
-        private static string GetNetCoreVersion()
-        {
-            try
-            {
-                var assembly = typeof(GCSettings).GetTypeInfo().Assembly;
-                var assemblyPath = assembly.CodeBase.Split(new[] {'/', '\\'}, StringSplitOptions.RemoveEmptyEntries);
-                var netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
-
-                if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
-                {
-                    return assemblyPath[netCoreAppIndex + 1];
-                }
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-
-            return "AssemblyVersionNotFound";
+            return $"digipost-api-client-dotnet/{assemblyVersion} ({RuntimeInformation.FrameworkDescription})";
         }
     }
 }
